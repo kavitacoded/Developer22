54fbf56759625b87c245962a73a891c41e758283
Yogesh <Yogesh@DESKTOP-HGICOLR>
2021-05-08 17:08:52 +0530

    productjava

---
+++ src/main/java/com/example/youtube/model/Product.java
@@ -0,0 +1,54 @@
+package com.example.youtube.model;
+
+public class Product {
+
+	
+	private int id;  
+	private String pname;  
+	private String batchno;  
+	private double price;  
+	private int noofproduct;  
+	
+	//getters and setters  
+	public int getId()   
+	{  
+	return id;  
+	}  
+	public void setId(int id)   
+	{  
+	this.id = id;  
+	}  
+	public String getPname()   
+	{  
+	return pname;  
+	}  
+	public void setPname(String pname)   
+	{  
+	this.pname = pname;  
+	}  
+	public String getBatchno()   
+	{  
+	return batchno;  
+	}  
+	public void setBatchno(String batchno)   
+	{  
+	this.batchno = batchno;  
+	}  
+	public double getPrice()   
+	{  
+	return price;  
+	}  
+	public void setPrice(double price)   
+	{  
+	this.price = price;  
+	}  
+	public int getNoofproduct()   
+	{  
+	return noofproduct;  
+	}  
+	public void setNoofproduct(int noofproduct)   
+	{  
+	this.noofproduct = noofproduct;  
+	}  
+	} 
+}
