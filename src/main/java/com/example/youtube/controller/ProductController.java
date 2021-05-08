de973e5d116870e64612addc930758667b513033
Yogesh <Yogesh@DESKTOP-HGICOLR>
2021-05-08 17:13:18 +0530

    sss

---
+++ src/main/java/com/example/youtube/controller/ProductController.java
@@ -0,0 +1,5 @@
+package com.example.youtube.controller;
+
+public class ProductController {
+
+}
---
+++ src/main/java/interfacee/IProductService.java
@@ -0,0 +1,7 @@
+package interfacee;
+
+public interface IProductService {
+	public interface IProductService   
+	{  
+	List<Product> findAll();  
+}
