fe133278f432be58153b88715fc6d799a781dacb
Yogesh <Yogesh@DESKTOP-HGICOLR>
2021-05-08 17:01:02 +0530

    start

---
+++ src/main/java/com/example/youtube/Developer22Application.java
@@ -0,0 +1,13 @@
+package com.example.youtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Developer22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Developer22Application.class, args);
+	}
+
+}
